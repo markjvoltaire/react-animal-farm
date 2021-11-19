0cb471e95e64ebcc93c216613d66f9fefd634522
Julie Nisbet <jmnisbet@gmail.com>
2021-11-19 10:35:03 -0800

    adding animal images and components

---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {animals} from '../../data.js';
+import Animal from '../Animal/Animal.js';
+import backgroundImg from '../../background.png'
+import './Main.css'
+
+export default function Main() {
+    return (
+        <main style={{backgroundImage: `url(${backgroundImg})`}}>
+            {animals.map(item => <Animal key={item.name} {...item}/>)}
+        </main>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const animals = [
+    { 
+        name: 'Elsie the Cow',
+        type: 'cow', 
+        says: 'Mooooooo!',
+        top: '200px',
+        left: '65%',
+    },
+    {
+        name: 'Noel the Donkey',
+        type: 'donkey',
+        says: 'Heee haw!',
+        top: '300px',
+        left: '45%',
+    },
+    {
+        name: 'Gary the Goose', 
+        type: 'goose',
+        says: 'Honk honk!',
+        top: '370px',
+        left: '75%',
+    },
+    {
+        name: 'Wilbur the Pig', 
+        type: 'pig',
+        says: 'Oink oink!',
+        top: '355px',
+        left: '15%'
+    },
+];
